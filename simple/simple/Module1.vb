68c04dd19e78ab2a5fac21260b9b23e480a7cd6f
opennetwork1997 <levbondarev1997@mail.ru>
2016-12-15 14:47:29 +0300

    addming code
    addimg code of tha applecatoin

---
+++ simple/simple/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+    Dim ar As Collection = New Collection
+    Sub Main()
+        ar.Add(2)
+        For i = 3 To 100000 step 2
+            For j = 1 To ar.Count \ 2 + 1
+                If i \ ar(j) = i / ar(j) Then
+                    GoTo er
+                End If
+            Next
+            Console.Write(i.ToString + ", ")
+            ar.Add(i)
+er:
+        Next
+        Console.ReadLine()
+    End Sub
+End Module
